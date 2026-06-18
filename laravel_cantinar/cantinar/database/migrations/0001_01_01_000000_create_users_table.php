5a7e2f657f996d11193d42756c225b0e67cd6fc6
1576167 <1576167@alu.murciaeduca.es>
2026-06-18 19:49:38 +0200

    Segundo Cambio

--- laravel_cantinar/cantinar/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel_cantinar/cantinar/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('color');
             $table->rememberToken();
             $table->timestamps();
         });
